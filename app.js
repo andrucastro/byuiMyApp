23bbd3aa6312f67559a08f349b18fd222b8a3920
Andres Castro <guevaracastroandres@gmail.com>
2021-10-16 11:59:51 -0500

    vamos con toda

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
-const path = require('path');
 const cors = require('cors')
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://andrucastro:Fabianvallejo01@cluster0.xfyy6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
-  User.findById('615e49fa7fafbaae4a59e05a')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -27,8 +54,15 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
@@ -36,6 +70,7 @@ const corsOptions = {
   origin: "https://e-commerce-andres.herokuapp.com/",
   optionsSuccessStatus: 200
 };
+
 app.use(cors(corsOptions));
 
 const options = {
@@ -49,26 +84,10 @@ const options = {
 const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://andrucastro:Fabianvallejo01@cluster0.xfyy6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose
-.connect(
-  MONGODB_URL, options
-)
-.then(result => {
-  User.findOne().then(user => {
-    if (!user) {
-      const user = new User({
-        name: 'andres',
-        email: 'guevaracastroandres@gmail.com',
-        cart: {
-          items: []
-        }
-      });
-      user.save();
-    }
+  .connect(MONGODB_URL, options)
+  .then(result => {
+    app.listen(process.env.PORT || 5000);
+  })
+  .catch(err => {
+    console.log(err);
   });
-  const PORT = process.env.PORT || 5000
-  app.listen(PORT);
-})
-.catch(err => {
-  console.log(err);
-});
-
